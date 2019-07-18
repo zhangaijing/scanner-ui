19c78b0efc66364389d97d7b1877142cee646218
yiyang <a56607529@163.com>
2019-07-18 20:39:27 +0800

    UI界面包

---
+++ src/main/java/com/zyy/scannerui/ScannerUiApplication.java
@@ -0,0 +1,13 @@
+package com.zyy.scannerui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScannerUiApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(ScannerUiApplication.class,args);
+    }
+
+}
---
+++ src/test/java/com/zyy/scannerui/ScannerUiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zyy.scannerui;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScannerUiApplicationTests {
+
+    @Test
+    public void contextLoads(){
+    }
+
+}
